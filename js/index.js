e4a65d8238c1c73af7266dcbde908a41318678a5
haibn <haibinyuy@berkeley.edu>
2024-10-13 17:00:27 -0700

    revisions

--- js/index.js
+++ js/index.js
@@ -1,22 +1,55 @@
+// Hides slider and show description of the clicked coffee image
+function description(coffee, data, image) {
+    let description = document.querySelector(".description");
+    let coffeeName = document.querySelector(".coffee-name");
+    let coffeeDescription = document.querySelector(".coffee-description");
+    let coffeeImage = document.querySelector(".coffee-image");
+    let coffeeSlider = document.querySelector(".coffees-slider")
+
+    coffeeImage.src = image;
+    coffeeImage.style.width = "300px";
+    coffeeName.textContent = coffee;
+    coffeeDescription.textContent = data;
+    coffeeSlider.style.display = "none";
+    description.style.display = "block";
+}
+
+// Hides description of coffee and return to slider
+let returnBtn = document.querySelector(".return");
+returnBtn.addEventListener("click", () => {
+    let description = document.querySelector(".description");
+    let coffeeSlider = document.querySelector(".coffees-slider")
+    description.style.display = "none";
+    coffeeSlider.style.display = "block";
+});
+
+// Fetch data asynchronosly from Coffee API
 async function getData() {
     try {
+        // Fech data
         let response = await fetch("https://api.sampleapis.com/coffee/hot");
         let data = await response.json();
 
         let coffees = document.querySelector(".coffees");
-        let coffeesSlider = document.querySelector(".coffees-slider");
 
+        // For each coffee fetched, create a coffee container with its image and title and append it to the slider 
         for (let i=0; i < data.length; i++) {
             let coffee = document.createElement("div");
             coffee.classList.add("coffee");
             coffee.style = `--position: ${i+1}`;
 
             let coffeeImg = document.createElement("img");
+            coffeeImg.classList.add("coffee-img");
             coffeeImg.src = `${data[i].image}`;
 
             let newLine = document.createElement("br");
             let coffeeName = document.createElement("h3");
-            coffeeName.textContent = `${data[i].title}`
+            let coffeNameData = `${data[i].title}`;
+            coffeeName.textContent = coffeNameData;
+
+            // If an image in the slider is clicked, run description function
+            coffeeImg.onclick = () => description(coffeNameData, data[i].description, `${data[i].image}`);
+
             coffeeName.style.textAlign = "center";
             coffee.append(coffeeImg);
             coffee.append(newLine);
